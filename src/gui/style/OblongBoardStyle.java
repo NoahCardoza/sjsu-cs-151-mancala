270fc1b8cfdd7702a4a9df121221868ea58d1854
NoahCardoza <noahcardoza@gmail.com>
2022-11-14 22:19:16 -0800

    fix: oblong style stones overflowing

--- src/gui/style/OblongBoardStyle.java
+++ src/gui/style/OblongBoardStyle.java
@@ -4,27 +4,24 @@ import java.awt.geom.RectangularShape;
 import java.awt.geom.RoundRectangle2D;
 
 public class OblongBoardStyle implements BoardStyle {
-
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "Oblong";
 	}
 
+	// TODO: some how increase padding of parents so children don't overflow with skinny rectangles
 	@Override
 	public RectangularShape getMancala(double x, double y, double w, double h) {
-		return new RoundRectangle2D.Double(x, y, w*0.9, h, 200, 20);
+		return new RoundRectangle2D.Double(x + w * 0.1 / 2, y, w*0.9, h, 200, 20);
 	}
 
 	@Override
 	public RectangularShape getPocket(double x, double y, double w, double h) {
-		// TODO Auto-generated method stub
-		return new RoundRectangle2D.Double(x, y, w*0.9, h, 200, 20);
+		return new RoundRectangle2D.Double(x + w * 0.1 / 2, y, w*0.9, h, 200, 20);
 	}
 
 	@Override
 	public RectangularShape getStone(double x, double y, double w, double h) {
-		// TODO Auto-generated method stub
 		return new RoundRectangle2D.Double(x, y, w, h, 200, 10);
 	}
 
