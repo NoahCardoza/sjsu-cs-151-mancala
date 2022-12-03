76d9fa08208f058f0283b29ba5a2d21efe73d237
c0ffee <platinumspoons@gmail.com>
2022-12-02 16:46:40 -0800

    added javadocs to board style

--- src/gui/style/OblongBoardStyle.java
+++ src/gui/style/OblongBoardStyle.java
@@ -9,23 +9,49 @@ package gui.style;
 import java.awt.geom.RectangularShape;
 import java.awt.geom.RoundRectangle2D;
 
+/**
+ * Oblong board style strategy for designing the board shape.
+ * @author Dominic Lopez
+ *
+ */
 public class OblongBoardStyle implements BoardStyle {
+	
+	/**
+	 * Name of the oblong board style
+	 * 
+	 * @return Name of the oblong board style
+	 */
 	@Override
 	public String getName() {
 		return "Oblong";
 	}
 
+	/**
+	 * Oval mancala shape for Oblong style
+	 * 
+	 * @return Oval mancala shape
+	 */
 	// TODO: some how increase padding of parents so children don't overflow with skinny rectangles
 	@Override
 	public RectangularShape getMancala(double x, double y, double w, double h) {
 		return new RoundRectangle2D.Double(x + w * 0.1 / 2, y, w*0.9, h, 200, 20);
 	}
 
+	/**
+	 * Oval pocket shape for Oblong style
+	 * 
+	 * @return Oval pocket shape
+	 */
 	@Override
 	public RectangularShape getPocket(double x, double y, double w, double h) {
 		return new RoundRectangle2D.Double(x + w * 0.1 / 2, y, w*0.9, h, 200, 20);
 	}
 
+	/**
+	 * Simple circle shape for Oblong style
+	 * 
+	 * @return Circle stone shape
+	 */
 	@Override
 	public RectangularShape getStone(double x, double y, double w, double h) {
 		return new RoundRectangle2D.Double(x, y, w, h, 200, 10);
--- src/gui/theme/OceanTheme.java
+++ src/gui/theme/OceanTheme.java
@@ -14,6 +14,10 @@ import java.awt.Font;
  *
  */
 public class OceanTheme implements BoardTheme {
+	
+	/**
+	 * Predefined colors for use in Ocean theme
+	 */
 	private static final Color boardBackgroundColor = new Color(117, 134, 210);
 	private static final Color pocketColor = new Color(75, 76, 80);
 	private static final Color deepPurple = new Color(89, 96, 205);
@@ -23,82 +27,166 @@ public class OceanTheme implements BoardTheme {
 	private static final Color armyGreen = new Color(99, 103, 69);
 	private static final Font font = new Font("Arial", Font.BOLD, 20);
 
+	/**
+	 * Name of the Ocean theme.
+	 * 
+	 * @return Returns the name of the Ocean theme.
+	 */
 	@Override
 	public String getName() {
 		return "Ocean";
 	}
 
+	/**
+	 * Background color of Ocean theme.
+	 * 
+	 * @return Returns color of the Ocean theme.
+	 */
 	@Override
 	public Color getBoardBackgroundColor() {
 		return boardBackgroundColor;
 	}
 
+	
+	
+	/**
+	 * Outline color of Ocean theme.
+	 * 
+	 * @return Returns outline color of the Ocean theme.
+	 */
 	@Override
 	public Color getBoardOutlineColor() {
 		return boardBackgroundColor;
 	}
 
 
+	/**
+	 * Pocket color of Ocean theme.
+	 * 
+	 * @return Returns pocket color of the Ocean theme.
+	 */
 	@Override
 	public Color getPocketColor() {
 		return pocketColor;
 	}
 
+	/**
+	 * Pocket outline color of Ocean theme.
+	 * 
+	 * @return Returns pocket outline color of the Ocean theme.
+	 */
 	@Override
 	public Color getPocketOutlineColor() {
 		return deepPurple;
 	}
 
+	/**
+	 * Active pocket outline color of Ocean theme.
+	 * 
+	 * @return Returns active pocket outline color of the Ocean theme.
+	 */
 	@Override
 	public Color getPocketActiveOutlineColor() {
 		return new Color(222, 127, 183);
 	}
 
+	/**
+	 * Text color of Ocean theme.
+	 * 
+	 * @return Returns text color of the Ocean theme.
+	 */
 	@Override
 	public Color getTextColor() {
 		return deepPurple;
 	}
 
+	/**
+	 * Stone color of Ocean theme.
+	 * 
+	 * @return Returns stone color of the Ocean theme.
+	 */
 	@Override
 	public Color getStoneColor1() {
 		return greenMud;
 	}
 
+	/**
+	 * Stone outline color of Ocean theme.
+	 * 
+	 * @return Returns stone outline color of the Ocean theme.
+	 */
 	@Override
 	public Color getStoneColor1Outline() {
 		return greenMud;
 	}
 
+	/**
+	 * Stone color 2 of Ocean theme.
+	 * 
+	 * @return Returns stone color 2 of the Ocean theme.
+	 */
 	@Override
 	public Color getStoneColor2() {
 		return armyGreen;
 	}
 
+	/**
+	 * Stone color 2 outline of Ocean theme.
+	 * 
+	 * @return Returns stone color 2 outline of the Ocean theme.
+	 */
 	@Override
 	public Color getStoneColor2Outline() {
 		return armyGreen;
 	}
 
+	/**
+	 * Stone color 3 of Ocean theme.
+	 * 
+	 * @return Returns stone color 3 of the Ocean theme.
+	 */
 	@Override
 	public Color getStoneColor3() {
 		return darkPurple;
 	}
 
+	/**
+	 * Stone color 3 outline of Ocean theme.
+	 * 
+	 * @return Stone color 3 outline of the Ocean theme.
+	 */
 	@Override
 	public Color getStoneColor3Outline() {
 		return darkPurple;
 	}
 
+	/**
+	   Stone color 4 of Ocean theme.
+	 * 
+	 * @return Returns stone color 4 of the Ocean theme.
+
+	 */
 	@Override
 	public Color getStoneColor4() {
 		return darkBlue;
 	}
 
+	/**
+	   Stone color 4 outline of Ocean theme.
+	 * 
+	 * @return Returns stone color 4 outline of the Ocean theme.
+
+	 */
 	@Override
 	public Color getStoneColor4Outline() {
 		return darkBlue;
 	}
 
+	/**
+	 * Font of Ocean theme.
+	 * 
+	 * @return Returns font of the Ocean theme.
+	 */
 	@Override
 	public Font getFont() {
 		return font;
